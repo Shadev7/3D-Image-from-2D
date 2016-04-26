3d5701dc2b405275a80a54d6287194350f7cfe54
Srivatsan Iyer <supersaiyanmode.rox@gmail.com>
2016-04-26 09:10:46 -0400

    Initial commit.

--- render.cpp
+++ render.cpp
@@ -4,6 +4,8 @@
 #include <vector>
 #include <iostream>
 #include <fstream>
+#include <algorithm>
+#include <iterator>
 #include <map>
 #include <math.h>
 #include <CImg.h>
@@ -12,53 +14,65 @@
 using namespace cimg_library;
 using namespace std;
 
-CImg<double> getDiparityImg (CImg<double> image_rgb,CImg<double> image_disp)
-{
-	int wid = image_rgb.width();
-	int ht = image_rgb.height();
-	double disparity_factor = 0.1;
-	CImg<double> diparityImg(wid, ht, 1, 3, 0);
-	//CImg<double> diparityImg = image_rgb;
-	int px_shift;
-	//cout << wid<< endl;
-	//cout << ht<< endl;
-	for (int i=0; i< wid ; i++)
-	{
-		for (int j=0; j< ht ; j++)
-		{
-			//cout << "1";
-			px_shift = (int)(round(image_disp.atXY(i,j)*disparity_factor));
-			//cout << image_disp.atXY(i,j);
-			//cout << px_shift << endl;
-		/* 	if (px_shift > 0)
-			{
-				cout << px_shift << endl;
-			} */
-			/* if (i+px_shift < 255)
-			{
-				//cout << i << "," <<j << endl;
-				//diparityImg.atXY(i,j) = 0;
-			} */
-			//else
-			//if (px_shift > 15 )	
-			{
-				//cout << "2";
-				//diparityImg.atXY(i,j) = image_rgb.atXY(i,(j - px_shift));
-				/* diparityImg.atXYZC(i, j, 0, 0) = image_rgb.atXYZC(i,(j - px_shift), 0, 0);
-                diparityImg.atXYZC(i, j, 0, 1) = image_rgb.atXYZC(i,(j - px_shift), 0, 1);
-                diparityImg.atXYZC(i, j, 0, 2) = image_rgb.atXYZC(i,(j - px_shift), 0, 2); */
-				/* diparityImg.atXYZC(i, j, 0, 0) = image_rgb.atXYZC((i + px_shift),j, 0, 0);
-                diparityImg.atXYZC(i, j, 0, 1) = image_rgb.atXYZC((i + px_shift),j, 0, 1);
-                diparityImg.atXYZC(i, j, 0, 2) = image_rgb.atXYZC((i + px_shift),j, 0, 2); */
-				diparityImg.atXYZC((i + px_shift), j, 0, 0) = image_rgb.atXYZC(i,j, 0, 0);
-                diparityImg.atXYZC((i + px_shift), j, 0, 1) = image_rgb.atXYZC(i,j, 0, 1);
-                diparityImg.atXYZC((i + px_shift), j, 0, 2) = image_rgb.atXYZC(i,j, 0, 2);
-			}
-			
+CImg<double> get_mask(const CImg<int>& depth, int depth) {
+	CImg<int> result(depth);
+	for (int y = 0; y < result.height(); y++) {
+		for (int x = 0; x < result.width(); x++) {
+			result.atXYZC(i, j, 0, 0) = result.atXYZC(x, y, 0, 0) == depth;
 		}
-		//cout << endl;
 	}
-	return diparityImg;
+	return result;
+}
+
+std::vector<int> get_unique_values(const CImg<int>& img) {
+	std::vector<int> unique;
+	
+	for (int y = 0; y < result.height(); y++) {
+		for (int x = 0; x < result.width(); x++) {
+			int pixel = result.atXYZC(x, y, 0, 0);
+			if (std::find(unique.begin(), unique.end(), pixel) == unique.end()) {
+				unique.push_back(pixel);
+			}
+		}
+	}
+	return unique;
+}
+
+CImg<double> multiply(const CImg<double>& input, const CImg<int>& mask) {
+	CImg<double> result(input);
+	
+	for (int y = 0; y < result.height(); y++) {
+		for (int x = 0; x < result.width(); x++) {
+			if (!mask.atXYZC(x, y, 0, 0)) {
+				result.atXYZC(x, y, 0, 0) = 0;
+			}
+		}
+	}
+	return result;
+}
+
+CImg<double> translate_image(const CImg<double>& input, int pixels) {
+	CImg<double> result(input.width(), input.height(), 1, 1, 0);
+
+	for (int y = 0; y < result.height(); y++) {
+		for (int x = 0; x < result.width(); x++) {
+			if (x + pixels < input.width()) {
+				result.atXYZC(x, y, 0, 0) = input.atXYZC(x + pixels, y, 0, 0);
+			}
+		}
+	}
+	return result;
+}
+
+CImg<double> depth_shift_image(const CImg<double>& input, const CImg<int>& depth) {
+	CImg<double> result(input.width(), input.height(), 1, 1, 0);
+	std::vector<int> unique_depths(get_unique_values(depth));
+	std::sort(unique_depths.begin(), unique_depths.end());
+	for (std::vector<int>::iterator it = unique_depths.begin(); it != unique_depths.end(); it++) {
+		if (!*it)
+			continue;
+
+	}
 }
 
 CImg<double> get3dImage (CImg<double> image_rgb,CImg<double> image_disp)
