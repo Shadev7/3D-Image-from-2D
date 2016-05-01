54bc4a6cdeb11a361ba140717643010d214f719b
Shashant Devadiga <ssdevadi@tank.soic.indiana.edu>
2016-05-01 02:57:34 -0400

    Part 1 temp

--- render.cpp
+++ render.cpp
@@ -66,12 +66,159 @@ CImg<int> translate_image(const CImg<int>& input, int pixels) {
 			}
 		}
 	}
+
 	return result;
 }
 
-CImg<int> depth_shift_image(const CImg<int>& input, const CImg<int>& depth, double factor) {
-	CImg<int> result(input);
-	result.get_shared_channel(0) = 0.0;
+CImg<double> nomalize_depth(const CImg<double>& depth) {
+	CImg<double> result(depth.width(), depth.height(), 1, 1, 0);
+	for (int y = 0; y < result.height(); y++) {
+		for (int x = 0; x < result.width(); x++) {
+			result.atXYZC(x, y, 0, 0) = 30*(depth.atXYZC(x, y, 0, 0) - 127.5)/127.5;
+		}
+	}
+	return result;
+}
+CImg<double> nomalize_depth2(const CImg<double>& depth) {
+	CImg<double> result(depth.width(), depth.height(), 1, 1, 0);
+	for (int y = 0; y < result.height(); y++) {
+		for (int x = 0; x < result.width(); x++) {
+			result.atXYZC(x, y, 0, 0) = -30*(depth.atXYZC(x, y, 0, 0) - 127.5)/127.5;
+		}
+	}
+	return result;
+}
+
+
+CImg<double> shifted_img3(const CImg<double>& input, const CImg<double>& norm_depth)
+{
+	//CImg<double> result(input.width(), input.height(), 1, 1, 3);
+	CImg<double> result(input.width(), input.height(), 1, 3, 0);
+	result.assign(input);
+	
+	
+	
+	for (int d = -10; d<= 10; d++)
+	{
+	for (int y = 0; y < result.height(); y++) {
+		for (int x = 0; x < result.width(); x++) {
+			if (norm_depth.atXYZC(x,y,0,0) >= d && norm_depth.atXYZC(x,y,0,0)< (d+1))
+			{
+				for (int c = 0; c < input.spectrum(); c++) {
+					result.atXYZC(x, y, 0, c) = input.atXYZC(x - norm_depth.atXYZC(x,y,0,0), y, 0, c);
+					//result.atXYZC(x + norm_depth.atXYZC(x,y,0,0), y, 0, c) = input.atXYZC(x, y, 0, c);
+				}
+			}
+		}
+
+	}
+	}
+}
+
+
+CImg<double> shifted_img(const CImg<double>& input, const CImg<double>& norm_depth)
+{
+	//CImg<double> result(input.width(), input.height(), 1, 1, 3);
+	CImg<double> result(input.width(), input.height(), 1, 3, 0);
+	//result.assign(input);
+	for (int d = -30; d<= 30; d++)
+	{
+	for (int y = 0; y < result.height(); y++) {
+		for (int x = 0; x < result.width(); x++) {
+			if (norm_depth.atXYZC(x,y,0,0) >= d && norm_depth.atXYZC(x,y,0,0)< (d+1))
+			{
+				for (int c = 0; c < input.spectrum(); c++) {
+					//result.atXYZC(x, y, 0, c) = input.atXYZC(x - norm_depth.atXYZC(x,y,0,0), y, 0, c);
+					result.atXYZC(x + norm_depth.atXYZC(x,y,0,0), y, 0, c) = input.atXYZC(x, y, 0, c);
+				}
+			}
+		}
+
+	}
+	}
+	
+	for (int y = 0; y < result.height(); y++) {
+		for (int x = 0; x < result.width(); x++) {
+			if (result.atXYZC(x, y, 0, 0) == 0 && result.atXYZC(x, y, 0, 1) ==  0 && result.atXYZC(x, y, 0, 2) == 0)
+			{
+				for (int c = 0; c < input.spectrum(); c++) {
+					//result.atXYZC(x, y, 0, c) = input.atXYZC(x + norm_depth.atXYZC(x,y,0,0), y, 0, c);
+					//result.atXYZC(x, y, 0, c) = input.atXYZC(x, y, 0, c);
+				}
+			}
+		}
+
+	}
+	//result.resize(input.width(), input.height(), 1, 3, 0);
+	return result;
+}
+CImg<double> shifted_img2(const CImg<double>& input, const CImg<double>& norm_depth)
+{
+	//CImg<double> result(input.width(), input.height(), 1, 1, 3);
+	CImg<double> result(input.width(), input.height(), 1, 3, 0);
+	//result.assign(input);
+	for (int d = -30; d<= 30; d++)
+	{
+	for (int y = 0; y < result.height(); y++) {
+		for (int x = 0; x < result.width(); x++) {
+			if (norm_depth.atXYZC(x,y,0,0) >= d && norm_depth.atXYZC(x,y,0,0)< (d+1))
+			{
+				for (int c = 0; c < input.spectrum(); c++) {
+					result.atXYZC(x, y, 0, c) = input.atXYZC(x - norm_depth.atXYZC(x,y,0,0), y, 0, c);
+					//result.atXYZC(x - norm_depth.atXYZC(x,y,0,0), y, 0, c) = input.atXYZC(x, y, 0, c);
+				}
+			}
+		}
+
+	}
+	}
+	
+	for (int y = 0; y < result.height(); y++) {
+		for (int x = 0; x < result.width(); x++) {
+			if (result.atXYZC(x, y, 0, 0) == 0 && result.atXYZC(x, y, 0, 1) ==  0 && result.atXYZC(x, y, 0, 2) == 0)
+			{
+				for (int c = 0; c < input.spectrum(); c++) {
+					//result.atXYZC(x, y, 0, c) = input.atXYZC(x + norm_depth.atXYZC(x,y,0,0), y, 0, c);
+					//result.atXYZC(x, y, 0, c) = input.atXYZC(x, y, 0, c);
+				}
+			}
+		}
+
+	}
+	return result;
+}
+CImg<double> get_anaglyph(const CImg<double>& input, const CImg<double>& shifted_image)
+{
+	CImg<double> result(input.width(), input.height(), 1, 3, 0);
+	//result.assign(input);
+	for (int y = 0; y < result.height(); y++) {
+		for (int x = 0; x < result.width(); x++) {
+			//result.atXYZC(x, y, 0, 0) = input.atXYZC(x, y, 0, 0);
+			result.atXYZC(x, y, 0, 1) = input.atXYZC(x, y, 0, 1);
+			result.atXYZC(x, y, 0, 2) = input.atXYZC(x, y, 0, 2);
+			//if (shifted_image.atXYZC(x, y, 0, 1) !=  0 || shifted_image.atXYZC(x, y, 0, 2) != 0)
+			{
+			result.atXYZC(x, y, 0, 0) = shifted_image.atXYZC(x, y, 0, 0);
+			//result.atXYZC(x, y, 0, 1) = shifted_image.atXYZC(x, y, 0, 1);
+			//result.atXYZC(x, y, 0, 2) = shifted_image.atXYZC(x, y, 0, 2);
+			}
+		}
+
+	}
+	//result.display("temp!");
+	return result;
+}
+CImg<double> depth_shift_image(const CImg<double>& input, const CImg<double>& depth, double factor) {
+	//CImg<double> result(input.width(), input.height(), 1, 1, 3);
+	CImg<double> result(input.width(), input.height(), 1, 3, 0);
+	CImg<double> norm_depth = nomalize_depth(depth);
+	CImg<double> norm_depth2 = nomalize_depth(depth);
+	CImg<double> shifted_image = shifted_img(input, norm_depth);
+	CImg<double> shifted_image2 = shifted_img2(input, norm_depth2);
+	
+	result = get_anaglyph(input, shifted_image);
+	//result = get_anaglyph(shifted_image2, shifted_image);
+	/*result.get_shared_channel(0) = 0.0;
 
 	std::vector<int> unique_depths(get_unique_values(depth));
 	std::sort(unique_depths.begin(), unique_depths.end());
@@ -83,7 +230,9 @@ CImg<int> depth_shift_image(const CImg<int>& input, const CImg<int>& depth, doub
 		//std::cout<<"Translating depths at: "<<*it<<" by "<<*it * factor<<" pixels"<<std::endl;
 		CImg<int> temp_img = translate_image(layer, *it * factor);
 		result.get_shared_channel(0) += temp_img.get_channel(0);
-	}
+	}*/
+	shifted_image.display("Shift!");
+	//result.display("Here!");
 	return result;
 }
 
@@ -99,10 +248,10 @@ int main(int argc, char *argv[]) {
 		factor = std::atof(argv[3]);
 	}
 
-	CImg<int> image_rgb(input_filename1.c_str());
-	CImg<int> image_disp(input_filename2.c_str());
+	CImg<double> image_rgb(input_filename1.c_str());
+	CImg<double> image_disp(input_filename2.c_str());
 
-	CImg<int> image_result = depth_shift_image(image_rgb, image_disp, factor);
+	CImg<double> image_result = depth_shift_image(image_rgb, image_disp, factor);
 	image_result.normalize(0,255);
 	image_result.save((input_filename1 + "-stereogram.png").c_str());
 	image_result.display("Look!");
